3a341ae0ea173dd5f07c725014e71ee3034b3e9e
Andrés Espinoza Bringas <aespinoza.bringas@gmail.com>
2019-10-16 17:04:23 -0300

    implementacion de create, update y delete

--- models/ubicacion.js
+++ models/ubicacion.js
@@ -1,17 +1,25 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
+
+var tiposDeUbicaciones = {
+    values: ['zona', 'departamento', 'seccion', 'pasillo', 'sala'],
+    message: '{VALUE} no es un tipo de ubicacion valido'
+};
 
 var ubicacionSchema = new Schema({
 
     nombre: { type: String, unique: true, required: [true, 'El nombre de la ubicacion es necesario'] },
     descripcion: { type: String, required: [true, 'La descripcion es necesaria'] },
     planta: { type: String, required: false, default: '' },
-    tipoUbicacion: { type: String, required: false },
-    //    vigente: { type: Boolean, required: [true, 'Se debe indicar si esta vigente la ubicacion'] },
-    //   fechaCreacion: { type: Date, required: true, default: Date.now },
-    //   fechaModificacion: { type: Date, required: true, default: Date.now },
+    tipoUbicacion: { type: String, required: true, enum: tiposDeUbicaciones },
+    vigente: { type: Boolean, required: [true, 'Se debe indicar si esta vigente la ubicacion'] },
+    fechaCreacion: { type: Date, required: true, default: Date.now },
+    fechaModificacion: { type: Date, required: true, default: Date.now },
 
 });
+
+ubicacionSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' });
 
 module.exports = mongoose.model('Ubicacion', ubicacionSchema);
--- routes/ubicacion.js
+++ routes/ubicacion.js
@@ -1,20 +1,152 @@
 var express = require('express');
 var app = express();
 var Ubicacion = require('../models/ubicacion');
+const logger = require('./../utils/logger');
 
 app.get('/', (req, res, next) => {
     Ubicacion.find({}, (err, ubicaciones) => {
         if (err) {
+            logger.info('Error al obtener ubicaciones');
             return res.status(500).json({
                 ok: false,
-                mensaje: 'Error ubicaciones!!',
+                mensaje: 'Error al obtener ubicaciones',
                 erros: err
             });
+
         }
+        logger.info('Obtiene ubicaciones');
         res.status(200).json({
             ok: true,
             ubicaciones: ubicaciones
         });
+    });
+
+});
+
+
+// ================================================
+// Actualizar un Ubicacion
+// ================================================
+
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Ubicacion.findById(id, (err, ubicacion) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar ubicacion',
+                errors: err
+            });
+        }
+        if (!ubicacion) {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'La ubicacion con el id: ' + id + 'no existe',
+                    errors: { message: 'No existe un ubicacion con ese ID' }
+                });
+            }
+        }
+
+
+        ubicacion.nombre = body.nombre;
+        ubicacion.descripcion = body.descripcion;
+        ubicacion.planta = body.planta;
+        ubicacion.tipoUbicacion = body.tipoUbicacion;
+
+        ubicacion.save((err, ubicacionGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar el ubicacion',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = ':)';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+
+        });
+
+
+
+    });
+
+});
+
+// ================================================
+// Crear un ubicacion
+// ================================================
+app.post('/', (req, res) => {
+
+    var body = req.body;
+
+    var ubicacion = new Ubicacion({
+        nombre: body.nombre,
+        descripcion: body.descripcion,
+        planta: body.planta,
+        tipoUbicacion: body.tipoUbicacion,
+        vigente: body.vigente,
+    });
+
+    ubicacion.save((err, ubicacionGuardada) => {
+        if (err) {
+            logger.info('Error al crear ubicacion: ' + err);
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al crear ubicacion',
+                errors: err
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            ubicacion: ubicacionGuardada,
+
+        });
+        logger.info('usuario creado: ' + ubicacion.nombre);
+    });
+
+
+});
+
+// ================================================
+// Eliminar usuario
+// ================================================
+
+app.delete('/:id', (req, res) => {
+    var id = req.params.id;
+
+    Ubicacion.findByIdAndRemove(id, (err, ubicacionBorrado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al borrar ubicacion',
+                errors: err
+            });
+        }
+
+        if (!ubicacionBorrado) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'No existe una ubicacion con ese ID',
+                errors: { message: 'No existe un ubicacion con el id: ' + id }
+            });
+        }
+
+        res.status(201).json({
+            ok: true,
+            ubicacion: ubicacionBorrado
+        });
+
     });
 
 });
