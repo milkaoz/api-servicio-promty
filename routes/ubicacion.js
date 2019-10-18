1c32e387afe9ad1b7083cee983c7a444e4536409
milkaoz <milkaoz@gmail.com>
2019-10-17 22:25:47 -0300

    Version estable metodos get, post, put y delete, con log implementado

--- routes/ubicacion.js
+++ routes/ubicacion.js
@@ -27,7 +27,6 @@ app.get('/', (req, res, next) => {
 
 });
 
-
 // ================================================
 // Crear un ubicacion
 // ================================================
@@ -69,11 +68,62 @@ app.post('/', (req, res) => {
 // ================================================
 
 app.put('/:id', (req, res) => {
-    logger.info('Inicia Put ubicacion');
-    var id = req.params.id;
 
+    var id = req.params.id;
+    var body = req.body;
 
     Ubicacion.findById(id, (err, ubicacion) => {
+        if (err) {
+            //logger.info('Error al buscar ubicacion con el id: ' + id);
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar ubicacion con el id: ' + id,
+                errors: { message: 'No existe un ubicacion con ese ID' }
+            });
+        }
+        if (!ubicacion) {
+            //logger.info('Error al buscar ubicacion con el id: ' + id);
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Error al buscar ubicacion con el id: ' + id,
+                errors: { message: 'No existe un ubicacion con ese ID' }
+            });
+        }
+
+        ubicacion.nombre = body.nombre;
+        ubicacion.descripcion = body.descripcion;
+        ubicacion.planta = body.planta;
+        ubicacion.tipoUbicacion = body.tipoUbicacion;
+        ubicacion.vigente = body.vigente;
+        ubicacion.fechaModificacion = Date.now();
+
+        ubicacion.save((err, ubicacionGuardada) => {
+            if (err) {
+                logger.info('Error al actualizar ubicacion con el id: ' + id);
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar ubicacion con el id: ' + id,
+                    errors: { message: 'Error al actualizar ubicacion con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                ubicacion: ubicacion,
+                mensaje: 'La ubicacion con el id: ' + id + ' ha sido actualizado correctamente',
+            });
+            logger.info('Ubicacion con el id: ' + id + ' actualiza correctamente');
+        });
+    });
+});
+
+
+/* 
+app.put('/:id', (req, res) => {
+    logger.info('Inicia Put ubicacion');
+    var id = req.params.id;
+    //logger.info('id: ' + id);
+
+    Ubicacion.findByIdAndUpdate(id, (err, ubicacion) => {
         logger.info('FindByid: ' + ubicacion);
         if (err) {
             logger.info('Error al intentar buscar la ubicacion: ' + err);
@@ -94,42 +144,46 @@ app.put('/:id', (req, res) => {
             }
         }
 
-        ubicacion.nombre = req.body.nombre;
-        ubicacion.descripcion = req.body.descripcion;
-        ubicacion.planta = req.body.planta;
-        ubicacion.tipoUbicacion = req.body.tipoUbicacion;
+        if (ubicacion != null) {
+            ubicacion.nombre = req.body.nombre;
+            ubicacion.descripcion = req.body.descripcion;
+            ubicacion.planta = req.body.planta;
+            ubicacion.tipoUbicacion = req.body.tipoUbicacion;
+            ubicacion.fechaModificacion = Date.now();
 
-        ubicacion.save((err, ubicacionGuardado) => {
-            if (err) {
-                logger.info('Error al actualizar la ubicacion ');
-                return res.status(400).json({
-                    ok: false,
-                    mensaje: 'Error al actualizar el ubicacion',
-                    errors: err
+            ubicacion.save((err, ubicacionGuardado) => {
+                if (err) {
+                    logger.info('Error al actualizar la ubicacion ');
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Error al actualizar el ubicacion',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: ubicacionGuardado,
+                    mensaje: 'Actualizacion ejecutada correctamente'
                 });
-            }
-
-
-            res.status(200).json({
-                ok: true,
-                usuario: usuarioGuardado,
-                mensaje: 'Actualizacion ejecutada correctamente'
+                logger.info('Actualizacion ejecutada correctamente ');
             });
-            logger.info('Actualizacion ejecutada correctamente ');
-        });
-
-
-
+        } else {
+            logger.info('La ubicacion con el id : ' + id + 'no existe');
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar ubicacion xxx',
+                errors: err
+            });
+        }
     });
 
 });
-
-
+*/
 
 // ================================================
 // Eliminar usuario
 // ================================================
-
 app.delete('/:id', (req, res) => {
     var id = req.params.id;
 
@@ -154,14 +208,13 @@ app.delete('/:id', (req, res) => {
 
         res.status(201).json({
             ok: true,
-            ubicacion: ubicacionBorrado
+            ubicacion: ubicacionBorrado,
+            mensaje: 'La ubicacion ' + req.params.nombre + ' con el id: ' + id + ' ha sido borrado correctamente',
         });
         logger.info('Ubicacion con el id: ' + id + ' Borrado correctamente');
 
     });
 
 });
-
-
 
 module.exports = app;
